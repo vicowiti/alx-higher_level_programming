b33f9aa6dbe765d1f9328cc0d52ea62a6d21e33c
Victor Owiti <viowiti12@gmail.com>
2022-06-30 14:16:48 -0700

    filter out '__'

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     my_array = dir(hidden_4).sort()
     for item in my_array:
-        print(item)
+        if not item.startswith("__"):
+            print(item)
