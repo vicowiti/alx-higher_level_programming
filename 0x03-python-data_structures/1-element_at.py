38d06d07e9dbc7d2ac8cc460921d32390c1691fb
Victor Owiti <viowiti12@gmail.com>
2022-07-02 07:04:39 -0700

    final

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif len(my_list) > idx:
+    elif idx >= len(my_list):
         return None
     else:
         return my_list[idx]
