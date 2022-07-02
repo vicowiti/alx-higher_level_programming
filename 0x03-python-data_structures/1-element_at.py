88d3db97db786e47771fcf1142a787bb1a704841
Victor Owiti <viowiti12@gmail.com>
2022-07-02 06:56:47 -0700

    none data type

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or len(my_list) > idx:
+    if idx < 0:
+        return None
+    elif len(my_list) > idx:
         return None
     else:
         return my_list[idx]
