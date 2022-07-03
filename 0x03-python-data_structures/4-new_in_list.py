a711a5de302dcbd771440d1d3a7760f0f5bab1e8
Victor Owiti <viowiti12@gmail.com>
2022-07-03 07:22:09 -0700

    copy list and replace elements

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_copy = my_list[:]
+    if idx < 0 or idx > len(list_copy):
+        return list_copy
+    else:
+        list_copy[idx] = element
+        return list_copy
