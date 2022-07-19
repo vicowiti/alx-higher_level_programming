d6d7e4371abd823893fe418e67a8b24828fce443
Victor Owiti <viowiti12@gmail.com>
2022-07-19 08:32:43 -0700

    test

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" """
+"""a module with a class handling getters and setters """
 
 
 class Square:
-    """ """
+    """a class handling getters and setters """
     def __init__(self, size=0):
+        """runs upon instantiation"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -14,14 +15,17 @@ class Square:
 
     @property
     def size(self):
+        """private size getter"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """setting the size"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """calculating the area"""
         return self.__size * self.__size
