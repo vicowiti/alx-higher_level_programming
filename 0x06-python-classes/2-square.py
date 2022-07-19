71542867841c04246bfe9e32ebf226b9abb26546
Victor Owiti <viowiti12@gmail.com>
2022-07-19 06:47:32 -0700

    test 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Raising exceptions in classes """
+
+
 class Square:
     """ A class that defines a square """
     def __init__(self, size=0):
         """ Runs upon instantiation """
         if size < 0:
             raise ValueError("size must be >= 0")
-        elif isinstance(size, int) != True:
+        elif type(size) != int:
             raise TypeError("size must be an integer")
         else:
             self.__size = size
