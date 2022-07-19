01833bbd2c4a64562a3c25ea9ed852f29bf407a3
Victor Owiti <viowiti12@gmail.com>
2022-07-19 07:18:54 -0700

    sqaure ofa  square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,17 @@
-#!/usr/bin/bash
-""" module defining  a class Square """
+#!/usr/bin/python3
+""" creates class Square """
 
 
 class Square:
-    """ a class defining a square """
+    """ Square class"""
     def __init__(self, size=0):
-        """ runs on instantiation """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
-        """ Method returning area of the square """
-        return self.__size ** 2
+    return self.__size * self.
+    
