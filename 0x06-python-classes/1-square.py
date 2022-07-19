c27bf4068441447849375f69970830fad6175f20
Victor Owiti <viowiti12@gmail.com>
2022-07-19 06:11:32 -0700

    class defining a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module defines a Square class """
+
+
+class Square:
+    """ A clas that defines an instance attribute size """
+
+    def __init__(self, size):
+        """ Method runs immidiately upon instatiation """
+
+        self.size = size
