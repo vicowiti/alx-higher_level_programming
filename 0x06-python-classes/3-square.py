1c2a1158cc966e44fdd2faf55081347f75254fa4
Victor Owiti <viowiti12@gmail.com>
2022-07-19 07:06:16 -0700

    add a method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+""" module defining  a class Square """
+
+
+class Square:
+    """ a class defining a square """
+    def __init__(self, size=0):
+        """ runs on instantiation """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """ Method returning area of the square """
+        return self.__size ** 2
